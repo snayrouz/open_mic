be1fd714a3a55861054c631273db88f80c4e5039
snayrouz <samuel.nayrouz@gmail.com>
2017-07-20 14:51:38 -0600

    Update

--- lib/joke.rb
+++ lib/joke.rb
@@ -1,11 +1,10 @@
 class Joke
+  attr_reader :id, :question, :answer
 
-  attr_accessor :id, :question, :answer
-
-  def initialize(joke ={id: nil, question: nil, answer: nil})
-    @id       = 1
-    @question = "Why did the strawberry cross the road?"
-    @answer   =   "Because his mother was in a jam."
+  def initialize(joke_hash)
+    @id       = joke_hash[:id]
+    @question = joke_hash[:question]
+    @answer   = joke_hash[:answer]
   end
 
 end
--- test/joke_test.rb
+++ test/joke_test.rb
@@ -6,9 +6,27 @@ require './lib/joke'
 class JokeTest < Minitest::Test
 
   def test_it_exist
-    j = Joke.new(joke = {id:nil, question:nil, answer:nil})
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
 
-    assert_instance_of Joke, j
+    assert_instance_of Joke, joke
+  end
+
+  def test_it_has_an_id
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+
+    assert_equal 1, joke.id
+  end
+
+  def test_it_has_a_question
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+
+    assert_equal "Why did the strawberry cross the road?" , joke.question
+  end
+
+  def test_it_has_an_answer
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+
+    assert_equal "Because his mother was in a jam.", joke.answer
   end
 
 end
